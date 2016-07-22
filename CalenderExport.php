9b8aaf171cf4ce685b1873ee4b29877c11744836
uguranyum <uguranyum@yandex.com>
2016-07-22 15:19:53 +0300

    example

--- CalenderExport.php
+++ CalenderExport.php
@@ -8,13 +8,13 @@ class CalenderExport{
     public function lookDb($list_id){
         $getLists    = Yii::$app->db2->createCommand("Select * From `calendar` Where `list_id` Like ".$list_id." And `availability` Not Like 'Available' ")->queryAll();
 
-        $ical   =  "BEGIN:VCALENDAR\nPRODID;X-RICAL-TZSOURCE=TZINFO:-//Wiflap Com//Wiflap Hosted Calendar 0.2//EN\nCALSCALE:GREGORIAN\nVERSION:2.0\n";
+        $ical   =  "BEGIN:VCALENDAR\nPRODID;X-RICAL-TZSOURCE=TZINFO:-//companyx Com//companyx Hosted Calendar 0.2//EN\nCALSCALE:GREGORIAN\nVERSION:2.0\n";
         foreach($getLists as $list){
             $dtstart = str_replace('-','',$list["booked_date"]);
             $ical .= "BEGIN:VEVENT \n";
             $ical .= "DTEND;VALUE=DATE:".$dtstart."\n";
             $ical .= "DTSTART;VALUE=DATE:".$dtstart."\n";
-            $ical .= "UID:Wiflap List Number : ".$list["list_id"]."\n";
+            $ical .= "UID:companyx List Number : ".$list["list_id"]."\n";
             $ical .= "DESCRIPTION: ".$list["availability"]."\n";
             $ical .= "SUMMARY: ".$list["notes"]."\n";
             $ical .= "END:VEVENT"."\n";
